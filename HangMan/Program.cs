bea7bd2b6a20eb6cd170860b0fa31602c477736d
ColtonMiller <ColtonJMMiller@gmail.com>
2015-04-15 19:29:35 -0600

    Added functions for word

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -14,17 +14,42 @@ namespace HangMan
         static int counterLives = 8;
         static string usersGuess = string.Empty;
         static bool playing = true;
+        static string hiddenWord = string.Empty;
+        static string actualWord = string.Empty;
         static void Main(string[] args)
         {
             StartScreen();
+            while (playing == true)
+            {
+                if (counterLives != 0)
+                {
+                    WordHidden();
+                    Console.ReadKey();  
+                }
+                else
+                {
+
+                }
+
+            }
+            Console.ReadKey();
         }
         static void StartScreen()
         {
-            Console.WriteLine("Welcome to Hangman what is your name user?");
+            
+            Console.WriteLine("                 Welcome to Hangman what is your name user?                     ");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.Write("                                     ");
             usersName = Console.ReadLine();
             Console.Clear();
-            Console.WriteLine("Welcome {0} You have 8 chances to guess the word using either a letter or by trying to guess the word itself",usersName);
+            Console.WriteLine("                               Welcome {0}!                                     ",usersName);
+            Console.WriteLine("");
+            Console.WriteLine("          You have 8 chances to guess the word using either a letter            ");
+            Console.WriteLine("                        or by trying to guess the word                          ");
+            Console.WriteLine("                         Press any key to continue...                           ");
             Console.ReadKey();
+            Console.Clear();
         }
         static bool ValidateUserInput(string userInput)
         {
@@ -35,6 +60,33 @@ namespace HangMan
                 return true;
             }
             return false;
+        }
+        public static void WordHidden()
+        {
+            int randomWordInt = rng.Next(0, possibleWords.Length);
+            actualWord = possibleWords[randomWordInt];
+            for (int i = 0; i < actualWord.Length; i++)
+            {
+                hiddenWord += "_";
+            }
+            Console.WriteLine(hiddenWord);
+        }
+        static void ChangeHiddenWord(string guess)
+        {
+            if (actualWord.Contains(guess))
+            {
+                if (guess.Length == 1)
+	            {
+                    char tempLetter = guess[0];
+                    for (int i = 0; i < actualWord.Length; i++)
+                    {
+                        if (actualWord[i] == tempLetter)
+                        {
+                            hiddenWord.Replace('_',actualWord[i] );
+                        }
+                    }
+                }
+            }
         }
     }   
 }
